d2b13bc4d5191dff3fe6b51a0f45877060d433f4
DOCENTE-A <DOCENTE-A@UCSP.AULAB>
2017-10-28 14:10:49 -0500

    relacion de mucho a muchos: prerequisitos

--- src/main/java/acad/model/Curso.java
+++ src/main/java/acad/model/Curso.java
@@ -1,18 +1,47 @@
 package acad.model;
 
+import java.util.List;
 import javax.persistence.Entity;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
 
 @Entity
 public class Curso {
   @Id
-  private String curso;
+  private String codigo;
 
-  public String getCurso() {
-    return curso;
+  private String nombre;
+
+  @ManyToMany
+  @JoinTable(name = "prerequisito",
+      joinColumns = @JoinColumn(name = "curso_codigo", referencedColumnName = "codigo"),
+      inverseJoinColumns = @JoinColumn(name = "prerequisito_codigo",
+          referencedColumnName = "codigo"))
+  private List<Curso> prerequisitos;
+
+  public String getNombre() {
+    return nombre;
   }
 
-  public void setCurso(String curso) {
-    this.curso = curso;
+  public void setNombre(String nombre) {
+    this.nombre = nombre;
+  }
+
+  public List<Curso> getPrerequisitos() {
+    return prerequisitos;
+  }
+
+  public void setPrerequisitos(List<Curso> prerequisitos) {
+    this.prerequisitos = prerequisitos;
+  }
+
+  public String getCodigo() {
+    return codigo;
+  }
+
+  public void setCodigo(String codigo) {
+    this.codigo = codigo;
   }
 }
